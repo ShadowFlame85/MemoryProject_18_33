82931a168fb181a1295fcb9d21492bcfc6c1f404
Shadow_Flame85 <wybren7@gmail.com>
2018-10-12 11:19:07 +0200

    ss

---
+++ WpfApp1 - Copy/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int ColNum = 6;
+        private const int RowNum = 6;
+        GameGrid grid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //grid = new GameGrid(MemoryGrid, ColNum, RowNum);
+        }
+
+       
+    }
+}
