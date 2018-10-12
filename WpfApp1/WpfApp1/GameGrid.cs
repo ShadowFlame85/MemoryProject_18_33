8233d401e52ec7cda8acc519a03aaa3180ca1977
Shadow_Flame85 <wybren7@gmail.com>
2018-10-12 11:16:55 +0200

    grid [NOT WORKING]

---
+++ WpfApp1/WpfApp1/GameGrid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+namespace WpfApp1
+{
+    public class GameGrid
+    {
+        public Grid grid;
+        public GameGrid(GameGrid grid, int cols, int rows)
+        {
+            //this.grid = grid;
+            InitializeMemoryGrid(cols, rows);
+
+        }
+        private void InitializeMemoryGrid(int cols, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+
+            }
+            for (int i = 0; i < cols; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            }
+        }
+    }
+}
