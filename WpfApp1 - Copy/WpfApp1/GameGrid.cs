9d237d89d2b74784a5658c4ee89b3adfd7433ee8
Cor Kooistra <corkie1997@hotmail.com>
2018-10-12 13:55:28 +0200

    image adding

--- WpfApp1 - Copy/WpfApp1/GameGrid.cs
+++ WpfApp1 - Copy/WpfApp1/GameGrid.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
 namespace WpfApp1
 {
     public class GameGrid
@@ -18,6 +20,21 @@ namespace WpfApp1
             this.rowNum = rows;
             InitializeMemoryGrid(cols, rows);
 
+        }
+
+        private void AddImages ()
+        {
+            for (int row =0; row < rowNum; row++)
+            {
+                for (int column =0; column < colNum; column++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = new BitmapImage(new Uri("@/Resources/Char/HarryPotter/Front1.jpg", UriKind.Relative));
+                    Grid.SetColumn(backgroundImage, column);
+                    Grid.SetRow(backgroundImage, row);
+                    grid.Children.Add(backgroundImage);
+                }
+            }
         }
         
         private void InitializeMemoryGrid(int cols, int rows)
